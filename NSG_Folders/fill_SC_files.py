c97ad9480859f1f07f039aa9f535abba348fbee6
im-vishalverma <160455284+im-vishalverma@users.noreply.github.com>
2024-09-27 16:47:53 +0530

    Update fill_SC_files.py

--- NSG_Folders/fill_SC_files.py
+++ NSG_Folders/fill_SC_files.py
@@ -1,11 +1,16 @@
+# STEP FOUR
+
 import shutil
 import os
-
-# Base directories for source files and destination folders
+# example subject IDs and folder names:
+folder_names = [ 'AA_20120815', 'AY_20111004', 'BQ_20120904', 'DH_20120806', 'EU_20120803', 'FE_20111010', 'FI_20120727', 'FJ_20120808', 'GC_20120803', 'IC_20120810', 'IS_20120809', 'NI_20120831', 'QL_20110925', 'LQ_20120814', 'QR_20111010', 'RF_20120809', 'RI_20110924', 'RI_20120815', 'RS_20120723', 'RT_20110925', 'SE_20110924', 'UB_20120806', 'UK_20110924', 'UK_20111004', 'XB_20120831',
+'AC_20120917', 'AR_20120813', 'CN_20120927', 'DA_20120813', 'DG_20120903', 'ER_20120816', 'FR_20120903', 'HA_20120813', 'IQ_20120904', 'JD_20120810', 'JH_20120925', 'JH_20121009', 'JL_20120927', 'JS_20120910', 'JZ_20120824', 'KI_20121009', 'NN_20120824', 'NN_20120831', 'OG_20120917', 'OK_20121011', 'OQ_20120925', 'RQ_20120903', 'RQ_20120917', 'YE_20120910' ]
+subject_ids = folder_names
+# Example Base directories for source files and destination folders
 source_base_dir = r"C:\Users\vhima\Downloads\SC_folder"
 destination_base_dir = r"C:\Users\vhima\Downloads\NSG_Folder"
 
-# Iterate over each subject_id
+# Iterate over each subject_id to move sc data to their respective "empty folders" subject-wise
 for subject_id in subject_ids:
     # Construct the source file path and the destination folder path
     file_path = rf"{source_base_dir}\{subject_id}_SC.mat"
