650ff30f0f70214a398a915cd8ff85a7d2fd7b5f
im-vishalverma <160455284+im-vishalverma@users.noreply.github.com>
2024-10-01 18:46:36 +0530

    Update using_STDOUT.py

--- NSG_Folders/using_STDOUT.py
+++ NSG_Folders/using_STDOUT.py
@@ -1,20 +1,16 @@
-
+import re
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 _=[]
 for i in np.arange(0.25,15.25,0.25):
         _.append(i)        
 
-
-import pandas as pd
-import re
-import matplotlib.pyplot as plt
 # Function to extract and parse array data
 def extract_arrays(file_content):
-    # Regex pattern to match arrays
     pattern = r'\[([^\]]+)\]'
     matches = re.findall(pattern, file_content)
-    
-    # Convert matches to list of lists of floats
     arrays = []
     for match in matches:
         array = list(map(float, match.split()))
@@ -22,30 +18,17 @@ def extract_arrays(file_content):
     
     return arrays
 
-graph=1
 Xi = np.zeros((60,60))
-# Read the file content
 
-for i in range(9):
-    with open(fr"C:\Users\vhima\Downloads\STDOUT ({i+5})", 'r') as file:
+for file_num in range(9):
+    with open(fr"C:\Users\vhima\Downloads\STDOUT ({file_num})", 'r') as file:
         file_content = file.read()
-
-    # Extract arrays
     arrays = extract_arrays(file_content)
-
-    # Create a DataFrame
     df = pd.DataFrame(arrays)
-
-    # Extract the first row
     first_row = df.iloc[:,-3]
-
-    # Reshape the first row into a 20x20 array
     reshaped_array = np.array(first_row).reshape((20, 20))
-
-    # Print the reshaped array
-#         print(reshaped_array)
-    file_num=i
     arr=reshaped_array
+    
     if file_num == 0:
         Xi[:20, :20 ] = arr
 
@@ -76,9 +59,12 @@ for i in range(9):
 plt.subplot(221)
 plt.imshow(Xi, 'bwr',aspect = 'auto',origin = 'lower')
 plt.axis('off')
-plt.title(f'{graph}')
 plt.colorbar()
 plt.show()
 alpha = np.where(Xi==np.min(Xi))
 print('glu = ', _[alpha[0][0]], ', gaba = ', _[alpha[1][0]])
-graph+=1
+plt.subplot(222)
+plt.scatter(gaba,glu)
+plt.title(f'glu={glu}, gaba={gaba}')
+plt.xlim(0.25,15.0)
+plt.lim(0.25,15.0)
